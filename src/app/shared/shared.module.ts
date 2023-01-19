c41a2b5fd99e839ea8a45bd4875dab8a8bb56408
Errol Lin <errollin@qq.com>
2023-01-19 23:46:56 +0800

    Add card component

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ComponentsModule } from './components/components.module';
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    ComponentsModule
+  ]
+})
+export class SharedModule { }
